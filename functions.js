eba0e9e9b4b10430abbc1520272a6a94a64586ca
EnPere <pereruba@gmail.com>
2022-11-29 18:17:14 +0100

    all files updated

---
+++ functions.js
@@ -0,0 +1,17 @@
+function changeBorder1() {
+  document.getElementById("mybox1").style.borderTopLeftRadius = "25px"
+}
+
+function changeBorder2() {
+  document.getElementById("mybox1").style.borderTopRightRadius = "40px"
+}
+
+function changeBorder3() {
+  document.getElementById("mybox1").style.borderBottomLeftRadius = "50px"
+}
+
+function changeBorder4() {
+  document.getElementById("mybox1").style.borderBottomRightRadius = "30px"
+}
+
+
